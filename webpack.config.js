e97a2e989e28e21fe08e5ed3494ca640e7923c06
Jon Shipman <jon@jonshipman.com>
2020-08-29 14:06:45 -0500

    added babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+const config = {
+  entry: ["./src/index.js"],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loaders: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
+
+module.exports = config;
